0b35b097143824cacdd20bde781868ad3d1a5d92
Jorgelu1s <mejiajimenez@gmail.com>
2019-05-30 00:19:20 -0500

    set SORS

--- config/application.rb
+++ config/application.rb
@@ -35,5 +35,12 @@ module Surveys
     config.generators do |g|
       g.orm :active_record, primary_key_type: :uuid
     end
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: [:get, :post, :options]
+      end
+    end
   end
 end
